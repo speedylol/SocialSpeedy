e0d024c48e660ac70145ceb3e12320ac22e0af4b
speedylol <malcolmspeed@ymail.com>
2014-05-09 09:57:31 -0400

    Update speedy.py

--- speedy.py
+++ speedy.py
@@ -4,7 +4,6 @@
 ### Add more Tumblr commands
 ### Add in Twitter API
 
-
 import facebook
 import sys
 import os
@@ -14,7 +13,6 @@ import pytumblr
 #go to developers.facebook.com/tools/explorer/ for your personal access token
 token = 'your token here'
 
-
 client = pytumblr.TumblrRestClient(
 	'n5r51TdkdkOeDdzf5aGA0GMPkobDiddHkytZU55CceSUNr1Rri',
 	'qUMJUNKoZfdomw5nUZ3kXD1SRFgXEnHCCJdleVlVAnZg1MxVCv',
@@ -22,90 +20,81 @@ client = pytumblr.TumblrRestClient(
 	'kBzG3Qu5h4BHaMNG204rorvnUoCDMFzyuPVgn7X9WtlIXRh7fd'
 )
 
-
 #function to restart script after a command is complete
 def restart():
 	py = sys.executable
 	os.execl(py, py, * sys.argv)
 
-
-print "1. Tumblr \n2. Facebook\n3. Quit"
-site_prompt = input("> ")
+command = input("> ")
 
 
 ##tumblr stuff
-def tumblr():
+class tumblr(self):
 
  	client.info()
 	print "Link your blog:  "
 	blog = raw_input("> ")
 	
 	print "1. Create text\n2. Create quote\n3. Create Link"
-	tumblr_prompt = input("> ")
 	
-	if tumblr_prompt == 1:
+	def post_text(self):
 		print "Whats on your mind? "
 		text = raw_input("> ")
 	
 		client.create_text(blog, body=text)
 		restart()
 
-	elif tumblr_prompt == 2:
+	def post_quote(self):
 		user_quote = raw_input("> ")
 		client.create_quote(blog, quote=user_quote)	
 		restart()
 
-	elif tumblr_prompt ==  3:
+	def post_link(self):
 		user_link = raw_input("> ")
 		client.create_link(blog, url=user_link)
 		restart()
 
 ##Facebook stuff
 
-def Fb():
+class Fb(object):
 
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object("me")
 	friends = graph.get_connections("me", "friends")
 
-	print "1.Update status \n2.Check amount of friends"
-
-	prompt = input("> ")
-
-	if prompt == 1 or prompt == 2:
-		pass
-	else:
-		assert False
-
-
 	while prompt != 'quit':
 
-	def update():
+	def update(self):
 		new_status = raw_input("> ")
 		graph.put_object("me", "feed", message=new_status )
-		prompt = 5		
 		restart()
 
-	##function doesn't work right now 
-	def number_friends():
 
-		friend_list  = len([friend['name'] for friend in friends['data']])
-		print friend_list
-		restart()
+if command == 'help':
+	print """
+		commands:
+		!fb status - posts a facebook status
+		!tumblr text - creates tumblr text post
+		!tumblr quote - creates tumblr quote post
+		!tumblr link - creates tumblr link post
+		quit - exits the program
+		"""
 
-	if prompt == "!fb status":
-		update()
+elif command == '!fb status':
+	command = fb()
+	command.status()
 
-	else:
-		assert False
+elif command == '!tumblr text':
+	command = tumblr()
+	command.post_text()
 
+elif command == '!tumblr quote':
+	command = tumblr()
+	command.post_quote()
+	
+elif command == '!tumblr link':
+	command = tumblr()
+	command.post_link()
 
-if site_prompt == 1:
-	tumblr()
-
-elif site_prompt == 2:
-	Fb()
-
-elif site_prompt == 3:
+elif command == 'quit':
 	sys.exit()
-
